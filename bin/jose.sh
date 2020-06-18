43da5e4ab295dcaeab20dc2e973ece9740cbc155
Quentin Castel <quentin.castel@yapily.com>
2020-06-18 18:01:52 +0100

    Fix jose.sh and logging

--- bin/jose.sh
+++ bin/jose.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-java -jar ${DIR}/jose-jwkset-cli.jar -Dspring.profiles.active=cli -Dspring.main.banner-mode=off $@
+java -Dspring.profiles.active=cli -Dspring.main.banner-mode=off -jar ${DIR}/jose-jwkset-cli.jar $@
