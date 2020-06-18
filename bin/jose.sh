eecd4a596dace358c6fbffc52b34f96244556a12
Quentin Castel <quentin.castel@yapily.com>
2020-06-18 17:17:14 +0100

    Fix jose.sh

--- bin/jose.sh
+++ bin/jose.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-java -jar jose-jwkset-cli.jar -Dspring.profiles.active=cli -Dspring.main.banner-mode=off $@
+java -jar ${DIR}/jose-jwkset-cli.jar -Dspring.profiles.active=cli -Dspring.main.banner-mode=off $@
